04bcb2d15e567b5593a02259c030693525897cb6
PAWEŁ <pawel.mlo@op.pl>
2020-11-13 14:52:28 +0100

    Dodano obsługę odejmownia

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,15 @@ public class Main {
     {
         return (a+b);
     }
+    static double Odejmownie(double a, double b)
+    {
+        return (a-b);
+    }
 
     public static void main(String[] args) {
         double a = 10,b = 5;
         System.out.println(Dodawanie(a,b));
+        System.out.println(Odejmownie(a,b));
 
     }
 }
